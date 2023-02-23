7c8d2c2abceae22972dbc9d088b7e7d9872184b1
trevor dewindt <trevor.dewindt@gmail.com>
2023-02-22 19:08:19 -0700

    commit tree 9064532e9fbabb59c4213e83bc5863bc916c3f26

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,12 @@
+require 'pry'
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot str = nil
+    if str
+        puts str
+        str
+    else
+        puts "Squawk!"
+        "Squawk!"
+    end
+end
